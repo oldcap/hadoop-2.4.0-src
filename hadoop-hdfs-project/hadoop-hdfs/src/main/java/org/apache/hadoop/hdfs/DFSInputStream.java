b6525b399e2a970ad9ba99e62c7167adb68126aa
oldcap <ZhangKong@ZhangKongs-iMac.local>
2014-05-28 23:06:32 -0400

    Fix syntax

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSInputStream.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSInputStream.java
@@ -262,7 +262,7 @@ implements ByteBufferReadable, CanSetDropBehind, CanSetReadahead,
 
   private long fetchLocatedBlocksAndGetLastBlockLength() throws IOException {
     final LocatedBlocks newInfo = dfsClient.getLocatedBlocks(src, 0);
-    LOG.info("[compose] newInfo: " + newInfo.locatedBlockCount());
+    DFSClient.LOG.info("[compose] newInfo: " + newInfo.locatedBlockCount());
     if (DFSClient.LOG.isDebugEnabled()) {
       DFSClient.LOG.debug("newInfo = " + newInfo);
     }

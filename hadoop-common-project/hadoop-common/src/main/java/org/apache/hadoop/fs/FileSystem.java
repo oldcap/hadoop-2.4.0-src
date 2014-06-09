8e02b824fcd8a7e2ff1e70218a2cee1790d4f866
oldcap <zhe.zhang.research@gmail.com>
2014-06-09 11:51:18 -0400

    More debugging

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/FileSystem.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/FileSystem.java
@@ -781,6 +781,7 @@ public abstract class FileSystem extends Configured implements Closeable {
    */
   public FSDataOutputStream create(Path f, boolean overwrite)
       throws IOException {
+        LOG.info("[compose] In FileSystem create(Path, boolean)")
     return create(f, overwrite, 
                   getConf().getInt("io.file.buffer.size", 4096),
                   getDefaultReplication(f),
